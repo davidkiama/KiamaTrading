39bf041ffe85b4a3e16b4ab31ec5db421ee3c1a5
David Kiama <davidkiama98@gmail.com>
2025-10-16 19:36:11 +0300

    Signals coded when Alpha pattern conditin is met

---
+++ chapter 2/import_excel_data.py
@@ -0,0 +1,25 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+
+# Example: get D1 (daily) data for BTC/USD, EUR/USD, etc.
+symbols = {
+    'EURUSD': 'EURUSD=X',
+    'USDCHF': 'USDCHF=X',
+    'GBPUSD': 'GBPUSD=X',
+    'USDCAD': 'USDCAD=X',
+    'BTCUSD': 'BTC-USD',
+    'ETHUSD': 'ETH-USD',
+    'XAUUSD': 'XAUUSD=X',
+    'XAGUSD': 'XAGUSD=X',
+    'SP500m': '^GSPC',
+    'UK100': '^FTSE'
+}
+
+# Pick one asset
+ticker = symbols['EURUSD']
+data = yf.download(ticker, start="2018-01-01", end="2025-10-15", interval="1d")
+
+# Save to Excel
+data.to_excel('my_data.xlsx')
